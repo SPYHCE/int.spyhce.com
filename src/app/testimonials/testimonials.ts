25c48703d217137391df058ed921f13e586d29e3
Florin Tomozei <florin.tomozei@spyhce.com>
2018-04-18 12:10:54 +0300

    added text and testimonials

--- src/app/testimonials/testimonials.ts
+++ src/app/testimonials/testimonials.ts
@@ -8,6 +8,13 @@ export const testimonials = [
       sunt convins ca o sa invat si mai mult de acum. Faptul ca lucram cu ultimele tehnologii ajuta foarte mult.
       Tehnologia evolueaza de la zi la zi, iar cel mai mult imi place ca noi tinem pasul cu ea si nu
       ramanem pe tehnologii vechi.
+    `,
+    translated_text: `
+      Spyhce is the second company at which I have worked until now and I feel that i have managed
+      to integrate myself a lot better here than at my previous company. Up until now, I have learned a lot of
+      new things and I am quite convinced that I will continue to learn a lot more. The fact that we work with
+      the latest technologies helps a lot. The technologies evolve a lot from day to day and I realy enjoy that
+      we (as a company) keep up with it.
     `
   },
   {
@@ -17,6 +24,11 @@ export const testimonials = [
       De cand fac parte din echipa Spyhce, simt ca investesc cu adevarat in cariera mea.
       Am gasit aici un mediu propice pentru invatare si dezvoltare. Desigur, lucrul acesta nu ar fi posibil
       fara colegi si fara proiectele interesante in care ne implicam.
+    `,
+    translated_text: `
+      Since I have been a part of the Spyhce company, I feel that I am really investing in my professional career.
+      Here, I have found a prosperous environment for learning and self-development. Of course, this would not be
+      possible without the colleagues and the interesting projects in which we are involved.
     `
   },
   {
@@ -27,6 +39,12 @@ export const testimonials = [
       unde am fost intampinata cu multa caldura de catre oameni minunati care au contribuit la dezvoltarea
       mea profesionala, dar si personala. Am avut ocazia sa cresc, sa invat lucruri noi, sa ma perfectionez
       iar toate acestea datorita persoanelor din jurul meu care mi-au oferit tot suportul de care am avut nevoie.
+    `,
+    translated_text: `
+      The first steps in everybody's career are the most important. I do consider that I was lucky to come to Spyhce,
+      where I was warmly greeted by my wonderful colleagues who have contributed to my professional development.
+      I had the opportunity to grow, learn new things and perfect myself in all of my duties because of the
+      persons around me who have offered their full support.
     `
   },
 ];
